c3140b92250ad0d7bc9bd17174d304acd0962cbc
SofiSit <sofisitjar@gmail.com>
2023-04-26 16:17:31 +0200

    initial server config

---
+++ server.ts
@@ -0,0 +1,10 @@
+const express = require ("express")
+const helmet = require ("helmet")
+const cors = require ("cors ")
+require ("dotenv").config()
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+

d4c1e1d1226494e59c9c51cb8ea8409f373ca279
Antonio Rufino <devs.toni@gmail.com>
2023-05-03 17:42:03 +0200

    finish log file and console

---
+++ src/config/logger/logfile.ts
@@ -0,0 +1,23 @@
+const { appendFile } = require("fs");
+const { dirname, join } = require("path");
+const appDir = dirname(require.main?.filename);
+const logPath: string = join(appDir, "../activity.log");
+
+export const logfile = {
+  write(data: string): void {
+    const date: string = new Date().toISOString().split("T")[0] as string;
+    const time: string = new Date().toISOString().split("T")[1] as string;
+    try {
+      appendFile(
+        logPath,
+        date + " " + time + " " + data + "\n",
+        "utf-8",
+        (err: object) => {
+          if (err) throw err;
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
--- src/config/logger/winston.ts
+++ src/config/logger/winston.ts
@@ -1,74 +1,77 @@
 import * as winston from "winston";
 
 const levels = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    http: 3,
-    debug: 4,
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
 };
 const colors = {
-    error: "red",
-    warn: "yellow",
-    info: "green",
-    verbose: "gray",
-    debug: "blue",
-    silly: "grey",
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "green",
+  debug: "blue",
 };
 
 const level = (): string => {
-    const env: string = (process.env['NODE_ENV'] as string) || "development";
-    const isDevelopment: boolean = env === "development";
-    return isDevelopment ? "debug" : "warn";
+  const env: string = (process.env["NODE_ENV"] as string) || "development";
+  const isDevelopment: boolean = env === "development";
+  return isDevelopment ? "debug" : "warn";
 };
 
 winston.addColors(colors);
 
 const consoleFormat = winston.format.combine(
-    winston.format.colorize({
-        all: true,
-    }),
-    winston.format.label({
-        label: "[LOGGER]",
-    }),
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm" }),
-    winston.format.align(),
-    winston.format.printf(
-        (info) => `${ info['timestamp']} ${ info.level }: ${ info.message }`
-    )
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.label({
+    label: "[DEBUG]",
+  }),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm" }),
+  winston.format.printf(
+    (info) => `${info["timestamp"]} ${info.level}: ${info.message}`
+  )
 );
 
 const logFormat = winston.format.combine(
-    winston.format.label({
-        label: "[LOGGER]",
-    }),
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm" }),
-    winston.format.align(),
-    winston.format.printf(
-        (info) => `${ info['timestamp']} ${ info.level }: ${ info.message }`
-    )
+  winston.format.label({
+    label: "[LOGGER]",
+  }),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm" }),
+  winston.format.printf(
+    (info) => `${info["timestamp"]} ${info.level}: ${info.message}`
+  )
 );
 
 const winstonLogger = winston.createLogger({
-    level: level(),
-    levels,
-    transports: [
-        new winston.transports.Console({
-            format: consoleFormat
-        }),
-        new winston.transports.File({
-            filename: "/error.log",
-            level: "error",
-            format: logFormat
-        }),
-        new winston.transports.File({
-            filename: "/all.log",
-            format: logFormat
-        }),
-    ],
+  level: level(),
+  levels,
+  transports: [
+    new winston.transports.Console({
+      format: consoleFormat,
+    }),
+    new winston.transports.File({
+      filename: "./error.log",
+      level: "error",
+      format: logFormat,
+    }),
+    new winston.transports.File({
+      filename: "./all.log",
+      format: logFormat,
+    }),
+  ],
 });
 
+export const logger = {
+  console: (message: string) => winstonLogger.debug(message),
+  error: (message: string) => winstonLogger.error(message),
+  warn: (message: string) => winstonLogger.warn(message),
+};
+
 export const loggerStream = {
-    write: (message: string) =>
-        winstonLogger.http(message.substring(0, message.lastIndexOf("\n"))),
+  write: (message: string) =>
+    winstonLogger.http(message.substring(0, message.lastIndexOf("\n"))),
 };

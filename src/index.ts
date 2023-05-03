52144d8d0a7bead7ca563d021ab3ff7fe97945cf
Antonio Rufino <devs.toni@gmail.com>
2023-05-03 17:42:03 +0200

    finish log file and console

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { connectDb } from "./db/connect";
 import CONFIGURATION from "./config/config";
 import { app } from "./server";
+import { logger } from "./config/logger/winston";
 
 const { app: env_app } = CONFIGURATION;
 
 connectDb()
   .then(() => {
-    console.log("Database connected!");
+    logger.console("Database connected!");
     app.listen(env_app.PORT, () => {
-      console.log("Application running on port " + env_app.PORT);
+      logger.console("Application running on port " + env_app.PORT);
     });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => logger.error(err));

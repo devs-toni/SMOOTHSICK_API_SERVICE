12b003411c5a8fd152cc7350d69b0ee6b2c6cf38
Psicowar <vbdam91@gmail.com>
2023-05-05 11:28:48 +0200

    fixed tokenGenerator function

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -9,11 +9,8 @@ export const UserController = {
     async register(req: Request, res: Response) {
         const params: FormRegister = req.body;
         const { form } = params;
-
-
         await bcrypt.hash(form.password, 10, (error: string, hash: string) => {
             if (error) throw error
-
             const user: IUserRegister = {
                 name: form.name,
                 last_name: form.lastname,
@@ -24,11 +21,9 @@ export const UserController = {
                 role: "U"
             }
             const saveUser = UserRepository.save(user)
-
             saveUser.then(user => {
                 if (typeof user === "undefined") return res.status(500).send('Error al registrar')
                 if (typeof user === 'number') return res.status(204).send('User exists');
-
                 return res.status(200).send(saveUser);
             })
 
@@ -44,21 +39,16 @@ export const UserController = {
             password: userData.password,
             id: userData.id
         }
-
         const currentUser = await UserRepository.get(user.email)
         if (typeof currentUser === "undefined") return res.status(401).send('Incorrect login data')
-
+        const token = await tokenGenerator(currentUser.id)
 
         await bcrypt.compare(userData.password, currentUser?.password, (error: string, result: boolean) => {
             if (error) throw error
-            if (result) {
-                const token = tokenGenerator(currentUser.id)
-                return res.status(200).send({ token, currentUser })
-            }
-
+            if (result) return res.status(200).send({ token, currentUser })
             if (!result) return res.status(401).send('Incorrect login data')
             return res.status(500).send("Something went wrong")
 
         })
-    }
+    },
 }

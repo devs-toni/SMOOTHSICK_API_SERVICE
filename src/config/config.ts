c35248d1846d5b191ac5dd2dc846a963c4299283
Psicowar <vbdam91@gmail.com>
2023-05-06 05:09:44 +0200

    added recovery pass method for send email to user

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,23 +6,23 @@ const { NODE_ENV, PORT, DB_URI, DB_URI_PROD } = process.env;
 export const ENV: env = (NODE_ENV as env) || "development";
 
 class CONFIGURATION implements Iconfig {
-  development: env_parameters = {
-    app: {
-      PORT: (PORT as unknown as number) || 4000,
-      FRONT_URI: "http://127.0.0.1:5173",
-    },
-    db: {
-      DB_URI: DB_URI as string,
-    },
-  };
-  production: env_parameters = {
-    app: {
-      PORT: (PORT as unknown as number) || 4000,
-      FRONT_URI: "https://smoothsick.arcprojects.es",
-    },
-    db: {
-      DB_URI: DB_URI_PROD as string,
-    },
-  };
+    development: env_parameters = {
+        app: {
+            PORT: PORT as unknown as number || 4000,
+            FRONT_URI: "http://localhost:5173"
+        },
+        db: {
+            DB_URI: DB_URI as string
+        }
+    }
+    production: env_parameters = {
+        app: {
+            PORT: PORT as unknown as number || 4000,
+            FRONT_URI: "https://smoothsick.arcprojects.es"
+        },
+        db: {
+            DB_URI: DB_URI as string
+        }
+    }
 }
 export default new CONFIGURATION()[ENV];

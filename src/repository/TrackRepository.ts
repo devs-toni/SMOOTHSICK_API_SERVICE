0630d694373b0a4db8fa694409f1b47a0c9d6e80
Sergi <scsanz87@gmail.com>
2023-05-08 18:58:07 +0200

    Playlist back end finished

--- src/repository/TrackRepository.ts
+++ src/repository/TrackRepository.ts
@@ -1,6 +1,7 @@
 import { ITrack } from "../models/Track";
 import { TrackModel } from "./schemas/Track";
 
+
 export const TrackRepository = {
   save: async (track: ITrack) => {
     const trackExist = await TrackModel.findOne({ id: track.id });
@@ -8,6 +9,8 @@ export const TrackRepository = {
       return await TrackModel.create(track);
     }
   },
+
+  findById: async (trackId: string) => await TrackModel.findOne({id: trackId}),
 
   findAll: async () => await TrackModel.find({}),
 

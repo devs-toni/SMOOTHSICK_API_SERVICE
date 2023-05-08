3bb408eb4d2211aac633f79050225357a5de269a
Antonio Rufino <devs.toni@gmail.com>
2023-05-08 19:23:20 +0200

    fix regex review in reset pass

--- src/controller/ResetPassController.ts
+++ src/controller/ResetPassController.ts
@@ -2,23 +2,22 @@ import { Request, Response } from "express";
 import { UserRepository } from "../repository/UserRepository";
 import { readdirSync } from "fs";
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-// const regExPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%!%*?&#]{8, 16}$/;
+const regExPassword =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 
 export const ResetPassword = {
-    async reset(req: Request, res: Response) {
-        const { pass, userId } = req.body;
-        // if (!regExPassword.test(pass)) return res.send({ validation: false });
-        try {
-            const newPass = await bcrypt.hash(pass, 10);
-            await UserRepository.FindByIdAndUpdate(userId, newPass)
-            res.status(201).send("Password updated successfully")
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Internal Server Error")
-        }
-
-
+  async reset(req: Request, res: Response) {
+    const { pass, userId } = req.body;
+    if (!regExPassword.test(pass)) return res.status(204).send({ validation: false });
+    try {
+      const newPass = await bcrypt.hash(pass, 10);
+      await UserRepository.FindByIdAndUpdate(userId, newPass);
+      res.send("Password updated successfully");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
     }
-}
+  },
+};

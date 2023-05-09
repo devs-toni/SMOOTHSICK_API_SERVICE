012041dac40099c1769b32b1656372a7cce4ba51
Antonio Rufino <devs.toni@gmail.com>
2023-05-09 19:16:26 +0200

    add upload and save uploaded songs

--- src/cloudinary/cloudinary.ts
+++ src/cloudinary/cloudinary.ts
@@ -6,3 +6,25 @@ cloudinary.config({
   api_key: CONFIGURATION.cloudinary.KEY,
   api_secret: CONFIGURATION.cloudinary.SECRET,
 });
+
+export const uploadImage = async (imagePath: string) => {
+  const imageUploaded = await cloudinary.uploader.upload(imagePath, {
+    resource_type: "image",
+    folder: "images/",
+    gravity: "east",
+    height: 500,
+    width: 500,
+    crop: "scale",
+    overwrite: true,
+  });
+  return imageUploaded;
+};
+export const uploadAudioFile = async (name: string, imagePath: string) => {
+  const imageUploaded = await cloudinary.uploader.upload(imagePath, {
+    resource_type: "video",
+    public_id: name,
+    folder: "audio/",
+    overwrite: true,
+  });
+  return imageUploaded;
+};
--- src/repository/TrackRepository.ts
+++ src/repository/TrackRepository.ts
@@ -1,30 +1,42 @@
 import { ITrack } from "../models/Track";
 import { TrackModel } from "./schemas/Track";
 
-
 export const TrackRepository = {
   save: async (track: ITrack) => {
-    const trackExist = await TrackModel.findOne({ id: track.id });
+    const trackExist = await TrackModel.findOne({ title: track.title });
     if (!trackExist) {
       return await TrackModel.create(track);
     }
   },
 
-  findById: async (trackId: string) => await TrackModel.findOne({id: trackId}),
+  findById: async (trackId: string) =>
+    await TrackModel.findOne({ id: trackId }),
 
-  findAll: async () => await TrackModel.find({}),
+  findAll: async () => await TrackModel.find({ disk_number: { $ne: -1 } }),
 
   findTopFour: async (artistId: string) =>
-    await TrackModel.find({ artist_id: artistId }).limit(4),
+    await TrackModel.find({
+      artist_id: artistId,
+      disk_number: { $ne: -1 },
+    }).limit(4),
 
   findBestSong: async (artistId: string) =>
-    await TrackModel.find({ artist_id: artistId }).sort({ rank: -1 }).limit(1),
+    await TrackModel.find({ artist_id: artistId, disk_number: { $ne: -1 } })
+      .sort({ rank: -1 })
+      .limit(1),
 
   findLikeById: async (playlistId: string, userId: string) =>
-    await TrackModel.find({ id: playlistId, likes: userId }),
+    await TrackModel.find({
+      id: playlistId,
+      likes: userId,
+      disk_number: { $ne: -1 },
+    }),
 
   findFavouritesByUserId: async (userId: string) =>
-    await TrackModel.find({ likes: userId }),
+    await TrackModel.find({ likes: userId, disk_number: { $ne: -1 } }),
+
+  findMySongs: async (userId: string) => 
+    await TrackModel.find({ disk_number: -1, artist_id: userId }),
 
   toggleLike: async (trackId: string, userId: string, operation: string) => {
     if (operation === "+") {
@@ -41,7 +53,10 @@ export const TrackRepository = {
   },
 
   search: async (query: string) =>
-    await TrackModel.find({ title: { $regex: query, $options: "i" } }),
+    await TrackModel.find({
+      title: { $regex: query, $options: "i" },
+      disk_number: { $ne: -1 },
+    }),
 
   deleteAll: async () => {
     const areDeleted = await TrackModel.deleteMany({});

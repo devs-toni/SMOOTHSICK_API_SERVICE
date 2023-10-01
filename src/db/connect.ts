874130186006fc3f4d46d67800085a45d119d99c
devs-toni <devs.toni@gmail.com>
2023-10-01 18:42:26 +0200

    feat: configure environments

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -7,7 +7,3 @@ const { db } = CONFIGURATION;
 export const connectDb = () => {
   return mongoose.connect(db.DB_URI);
 };
-
-/* export const connectDb = () => {
-    return mongoose.connect(`${process.env.DB_URI_PROD_KEY}${__dirname}/mongo_key.pem`);
-}; */
